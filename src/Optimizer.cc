afff11374954c6320f86a0f725877a2bf1a34980
smzmsys <shimizu@bi.c.titech.ac.jp>
2024-10-08 09:16:17 +0000

    refactoring

--- src/Optimizer.cc
+++ src/Optimizer.cc
@@ -11,7 +11,7 @@ namespace fragdock {
     const int NEAREST_NUM = 200;
     fltype trans_step = 0.5;
     fltype rotate_step = pi / 30.0;
-    Molecule first_mol = mol;
+    Molecule initial_mol = mol;
     while(true) {
       fltype next_val = 1e10;
       Molecule next_mol = mol;
@@ -31,7 +31,7 @@ namespace fragdock {
         tmp_mol.translate(center + dv);
         // tmp_mol.translate(dv);
 
-        if (first_mol.calcRMSD(tmp_mol) > max_rmsd) continue;
+        if (initial_mol.calcRMSD(tmp_mol) > max_rmsd) continue;
 
         fltype val = ec.getEnergy(tmp_mol, receptor);
         if(val < next_val) {
@@ -59,7 +59,7 @@ namespace fragdock {
     const int NEAREST_NUM = 200;
     fltype trans_step = 0.5;
     fltype rotate_step = pi / 30.0;
-    Molecule first_mol = mol;
+    Molecule initial_mol = mol;
     while(true) {
       fltype next_val = 1e10;
       Molecule next_mol = mol;
@@ -79,7 +79,7 @@ namespace fragdock {
         tmp_mol.translate(center + dv);
         // tmp_mol.translate(dv);
 
-        if (first_mol.calcRMSD(tmp_mol) > max_rmsd) continue;
+        if (initial_mol.calcRMSD(tmp_mol) > max_rmsd) continue;
 
         fltype val = calcInterEnergy(tmp_mol) + mol.getIntraEnergy();
         if(val < next_val) {
